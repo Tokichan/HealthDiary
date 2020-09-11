04778e3b84b602a8523e3fccb53b5259519fd9d7
Tokichan <kev3058989@gmail.com>
2020-09-11 15:57:59 +0800

    First commit

--- app/src/main/java/toki/sole/healthdiary/RecordInputInterface.java
+++ app/src/main/java/toki/sole/healthdiary/RecordInputInterface.java
@@ -1,21 +1,43 @@
 package toki.sole.healthdiary;
 
-import android.app.AlertDialog;
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.ArrayList;
+
 public class RecordInputInterface extends DialogFragment {
+    ArrayList<String> types;
+    public RecordInputInterface(ArrayList<String> types){
+        this.types = types;
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        Dialog dialog = new Dialog(getContext(),android.R.style.Theme_NoTitleBar_Fullscreen);
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Spinner typeSpinner = (Spinner)getActivity().findViewById(R.id.type_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),R.layout.record_input,R.id.SpinnerTextView,this.types);
+        typeSpinner.setAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
